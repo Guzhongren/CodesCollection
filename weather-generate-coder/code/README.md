3258ac055a1be6c1995b9816778dd564541e1ec5
guzhongren <guzhongren@live.cn>
2017-10-18 15:00:05 +0800

    add resource; 基于Python,针对气象渲染文件读取的代码生成器

---
+++ weather-generate-coder/code/README.md
@@ -0,0 +1,11 @@
+### 使用说明
+> 打开py文件，将第一行的运行环境的路径指定正确后再运行。
+#### 数据
+执行数据放在weater/pal中
+
+#### 执行
+```shell
+./process-X.py fileName1[ fileName2[ fileName3 ...]]
+```
+#### 执行结果
+在weater/code_txt中
---
+++ weather-generate-coder/code/process.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python
+# code ..
+from itertools import islice
+import sys
+#pal_name = 'W-01'
+print("begin process.....")
+for i in range(1,len(sys.argv)):
+    pal_name = sys.argv[i]
+    print("process " + pal_name)
+    with open('../pal/'+pal_name+'.pal') as file:
+        with open("../code_txt/"+pal_name+"_CODE.txt", 'w') as code_file:
+            code_file.write("switch(value) {\n")
+            for line in islice(file,1,None):
+                row = line.split()
+                #print(row)
+                temp = "case " + str(int(row[0])-1)+ ":\n" +"    " + "color = new Color("+ str(int(row[0])-1)+", "+ row[1]+ ", " +row[2]+ ","+ row[3]  +");\n" + "    break;\n"
+                code_file.write(temp)
+            code_file.write("default:\n    color = new Color(0, 0, 0, 0);\n     break;")
+            code_file.write("\n}")
+    print('process '+ pal_name+' finished...')
